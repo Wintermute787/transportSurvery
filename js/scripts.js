89033d39d6d9cc507b66b6dc212956be61604962
Joe Barnes and Marina Poltorak <marina@>
2019-04-02 13:37:42 -0700

    add capitalization to user responses list

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,20 @@ $(document).ready(function(){
   $("form#transportation_survey").submit(function(event) {
     event.preventDefault();
       $("#work-responses").show();
+
       $("input:checkbox[name=work-transportation]:checked").each(function(){
+
         var workTransportationMode = $(this).val();
-        $('#work-responses').append(workTransportationMode + "<br>");
+        var capitalWorkTransportation = workTransportationMode.toUpperCase();
+        $('#work-responses').append(capitalWorkTransportation + "<br>");
+
       });
+
       $("#fun-responses").show();
       $("input:checkbox[name=fun-transportation]:checked").each(function(){
         var funTransportationMode = $(this).val();
-        $('#fun-responses').append(funTransportationMode + "<br>");
+        var capitalFunTransportationMode = funTransportationMode.toUpperCase();
+        $('#fun-responses').append(capitalFunTransportationMode + "<br>");
       });
         $('#transportation_survey').hide();
     });
